9a10525b3a74e21130882e7604ffa16e3cc5b486
jlamberti <jlamberti2015@gmail.com>
2018-05-08 17:24:18 -0600

    Removed unnecessary comments.

--- src/index.js
+++ src/index.js
@@ -37,28 +37,6 @@ class Board extends React.Component {
             </div>
         );
     }
-
-    // render() {
-    //     return (
-    //         <div>
-    //             <div className="board-row">
-    //                 {this.renderSquare(0)}
-    //                 {this.renderSquare(1)}
-    //                 {this.renderSquare(2)}
-    //             </div>
-    //             <div className="board-row">
-    //                 {this.renderSquare(3)}
-    //                 {this.renderSquare(4)}
-    //                 {this.renderSquare(5)}
-    //             </div>
-    //             <div className="board-row">
-    //                 {this.renderSquare(6)}
-    //                 {this.renderSquare(7)}
-    //                 {this.renderSquare(8)}
-    //             </div>
-    //         </div>
-    //     );
-    // }
 }
 
 class Game extends React.Component {
